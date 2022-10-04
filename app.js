3d3fa74deda543e3ad44f62b36386cb4fe9cc0ec
Habaya76 <habayatou93@gmail.com>
2022-10-04 12:26:51 +0200

    deuxieme commit

--- app.js
+++ app.js
@@ -1,9 +1,26 @@
 
 const express = require('express')
+const { success } = require('./helper.js')
+let pokemons = require('./mock-pokemon')
 
 const app = express()
 const port = 3000
 
 app.get('/',(req,res) => res.send('hello, Express !!'))
+
+app.get('/api/pokemons/:id', (req,res) => {
+    const id = parseInt(req.params.id)
+    const pokemon = pokemons.find(pokemon => pokemon.id === id)
+    const message = 'un pokemon a bien été trouvé'
+    res.json(success(message, pokemon))
+})
+// retourne la liste des pokémons au format JSON, avec un message :
+app.get('/api/pokemons', (req, res) => {
+    const message = 'La liste des pokémons a bien été récupérée.'
+    res.json(success(message, pokemons)) 
+  })
+   
+
+
 
 app.listen(port,() => console.log(`Notre aplication node est demarrée sur : http://localhost:${port}`))
